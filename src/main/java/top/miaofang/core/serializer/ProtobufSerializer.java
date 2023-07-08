9a66a5545b310eaf398f658991f90d605f06e041
Miao Fang <sunnymiaofang@gmail.com>
2023-07-08 13:37:20 -0700

    Added protobuf serializer

--- src/main/java/top/miaofang/core/serializer/ProtobufSerializer.java
+++ src/main/java/top/miaofang/core/serializer/ProtobufSerializer.java
@@ -1,64 +1,64 @@
 package top.miaofang.core.serializer;
 
-//import io.protostuff.LinkedBuffer;
-//import io.protostuff.ProtostuffIOUtil;
-//import io.protostuff.Schema;
-//import io.protostuff.runtime.RuntimeSchema;
-//import top.guoziyang.rpc.enumeration.SerializerCode;
-//
-//import java.util.Map;
-//import java.util.Objects;
-//import java.util.concurrent.ConcurrentHashMap;
-//
-///**
-// * 使用ProtoBuf的序列化器
-// * @author miao
-// */
-//public class ProtobufSerializer implements CommonSerializer {
-//
-//    private LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
-//    private Map<Class<?>, Schema<?>> schemaCache = new ConcurrentHashMap<>();
-//
-//    @Override
-//    @SuppressWarnings("unchecked")
-//    public byte[] serialize(Object obj) {
-//        Class clazz = obj.getClass();
-//        Schema schema = getSchema(clazz);
-//        byte[] data;
-//        try {
-//            data = ProtostuffIOUtil.toByteArray(obj, schema, buffer);
-//        } finally {
-//            buffer.clear();
-//        }
-//        return data;
-//    }
-//
-//    @Override
-//    @SuppressWarnings("unchecked")
-//    public Object deserialize(byte[] bytes, Class<?> clazz) {
-//        Schema schema = getSchema(clazz);
-//        Object obj = schema.newMessage();
-//        ProtostuffIOUtil.mergeFrom(bytes, obj, schema);
-//        return obj;
-//    }
-//
-//    @Override
-//    public int getCode() {
-//        return SerializerCode.valueOf("PROTOBUF").getCode();
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    private Schema getSchema(Class clazz) {
-//        Schema schema = schemaCache.get(clazz);
-//        if (Objects.isNull(schema)) {
-//            // 这个schema通过RuntimeSchema进行懒创建并缓存
-//            // 所以可以一直调用RuntimeSchema.getSchema(),这个方法是线程安全的
-//            schema = RuntimeSchema.getSchema(clazz);
-//            if (Objects.nonNull(schema)) {
-//                schemaCache.put(clazz, schema);
-//            }
-//        }
-//        return schema;
-//    }
-//
-//}
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+import top.miaofang.common.enumeration.SerializerCode;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 使用ProtoBuf的序列化器
+ * @author miao
+ */
+public class ProtobufSerializer implements CommonSerializer {
+
+    private LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+    private Map<Class<?>, Schema<?>> schemaCache = new ConcurrentHashMap<>();
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public byte[] serialize(Object obj) {
+        Class clazz = obj.getClass();
+        Schema schema = getSchema(clazz);
+        byte[] data;
+        try {
+            data = ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } finally {
+            buffer.clear();
+        }
+        return data;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object deserialize(byte[] bytes, Class<?> clazz) {
+        Schema schema = getSchema(clazz);
+        Object obj = schema.newMessage();
+        ProtostuffIOUtil.mergeFrom(bytes, obj, schema);
+        return obj;
+    }
+
+    @Override
+    public int getCode() {
+        return SerializerCode.valueOf("PROTOBUF").getCode();
+    }
+
+    @SuppressWarnings("unchecked")
+    private Schema getSchema(Class clazz) {
+        Schema schema = schemaCache.get(clazz);
+        if (Objects.isNull(schema)) {
+            // 这个schema通过RuntimeSchema进行懒创建并缓存
+            // 所以可以一直调用RuntimeSchema.getSchema(),这个方法是线程安全的
+            schema = RuntimeSchema.getSchema(clazz);
+            if (Objects.nonNull(schema)) {
+                schemaCache.put(clazz, schema);
+            }
+        }
+        return schema;
+    }
+
+}

666f20400ffb1b58ffc56d24d7aa408172969faa
Miao Fang <sunnymiaofang@gmail.com>
2023-07-09 10:33:31 -0700

    Fixed can't find serializer error

--- src/main/java/top/miaofang/test/client/NettyTestClient.java
+++ src/main/java/top/miaofang/test/client/NettyTestClient.java
@@ -5,6 +5,9 @@ import top.miaofang.api.HelloService;
 import top.miaofang.core.client.RpcClient;
 import top.miaofang.core.client.RpcClientProxy;
 import top.miaofang.core.netty.client.NettyClient;
+import top.miaofang.core.serializer.KryoSerializer;
+import top.miaofang.test.server.HelloServiceImpl;
+
 
 /**
  * 测试用Netty消费者
@@ -14,6 +17,7 @@ public class NettyTestClient {
 
     public static void main(String[] args) {
         RpcClient client = new NettyClient("127.0.0.1", 9999);
+        client.setSerializer(new KryoSerializer());
         RpcClientProxy rpcClientProxy = new RpcClientProxy(client);
         HelloService helloService = rpcClientProxy.getProxy(HelloService.class);
         HelloObject object = new HelloObject(12, "This is a message");
